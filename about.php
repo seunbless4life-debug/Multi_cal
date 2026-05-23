6cd0046cc33ed6e496282f15375cd2dbdc33afe3
Lasaki <seunbless4life@gmail.com>
2026-05-23 16:03:50 +0100

    Lm 2.04

--- about.php
+++ about.php
@@ -2,3 +2,7 @@
     require_once 'header.php';
     $page_title = "About";
 ?> 
+<div class="abtus_main_pic">
+    <h3>About Us</h3>
+    <p>Lm is an application designed by Lasaki Seun a sudent of Roncloud Technology. To help creating a calculatorapplication that is user-friendly and efficient. My goal is to provide a platform that helps users manage their expense and stay organized .</p>
+</div>
