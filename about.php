0aa9a13f1ebf852663f1a94b0cbf4030b9a1b171
Lasaki Oluwaseun <seunbless4life@gmail.com>
2026-07-08 23:20:39 +0100

    new

--- about.php
+++ about.php
@@ -8,3 +8,8 @@
     <h3>About Us</h3>
     <p>Lm is an application designed by Lasaki Seun a sudent of Roncloud Technology. To help creating a calculatorapplication that is user-friendly and efficient. My goal is to provide a platform that helps users manage their expense and stay organized.</p>
 </div>
+<div>
+    <div>
+        
+    <div>
+<div>
